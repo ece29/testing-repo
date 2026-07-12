edc5032555b083b612c52e04f860a0c2bcd694b1
Prince Sahgal <sahgalanubhav3@gmail.com>
2026-07-12 17:57:00 +0530

    changes

---
+++ file.js
@@ -0,0 +1,234 @@
+
+// // a// // // // console.log(`file is working`)
+// //     // // // // console.log(undefined)
+
+
+
+// //     // // // // / console.log(undefined)
+// //     // // // // // console.log(null)
+
+// //     // // // // console.log({sanket:123})
+
+
+
+
+// //     // // // // console.log(typeof 2+3)
+// //     // // // // console.log(typeof null);
+// //     // // // // console.log(typeof undefined);
+
+
+
+// //     // // // // console.log(5 & 3)
+// //     // // // // console.log(8 | 1)
+// //     // // // // console.log(3 ^3);
+// //     // // // // console.log(5 << 2)
+// //     // // // // console.log(5 >> 1)
+
+
+// //     // // // // console.log("sanket"+"string")
+// //     // // // // console.log(0 |-0)
+
+// //     // // // // console.log(-0 >> 0);
+
+
+// //     // // // // // console.log("5" >> 1);
+// //     // // // // // ?:--- ternary operator
+// //     // // // // // (condition) ? true : false  
+
+
+
+// //     // // // // console.log((2<1) ? true : false)
+
+
+// //     // // // // // statement and expression
+
+
+// //     // // // // let a=10;
+// //     // // // // a=1+(x+2);
+
+
+// //     // // // // conditionals
+
+// //     // // // // condition - decsion and decison 2
+
+
+
+
+// //     // // // // if else -else if
+// //     // // // // if(conditon){
+
+// //     // // // // }
+// //     // // // // else{
+
+// //     // // // // }
+
+// //     // // // // if (conditon){
+
+// //     // // // // }
+// //     // // // // else if(condtion 2){
+
+// //     // // // // }
+// //     // // // // else if(condtion 2){
+
+// //     // // // // }
+// //     // // // // else {
+
+// //     // // // // }
+
+
+// //     // // // // 
+// //     // // // // for(let i=0;i<5;i++){
+
+// //     // // // // if(i==3){
+// //     // // // // break;
+// //     // // // // }
+// //     // // // // console.log(i);
+// //     // // // // }
+
+
+// //     // // // let exp ="10";
+// //     // // // let a=10;
+// //     // // // let b=20;
+
+// //     // // // switch(exp){
+// //     // // //     case "10":
+// //     // // //     console.log(a+b); 
+// //     // // //     // break;
+// //     // // //     // case"-":
+// //     // // //     // console.log(a-b);
+// //     // // //     // break;
+// //     // // //     default:
+// //     // // //     console.log("invalid operation");
+
+// //     // // // }
+
+
+// //     // // // function todo(task){
+// //     // // //     setTimeout(function func() {
+// //     // // //         console.log("task is optional",task);}
+// //     // // //     ,2000)
+
+// //     // // // }
+
+// //     // // // function a(name){
+// //     // // //     return function b(){
+// //     // // //         console.log(name);
+// //     // // //     }
+// //     // // // }
+
+// //     // // // let x=a("sanket")
+// //     // // // console.log(x)
+// //     // // // x()
+
+// //     // // // const add=(function (){
+// //     // // //     let counter=0;
+// //     // // //     return function(){
+// //     // // //         counter++;
+// //     // // //         return counter;
+// //     // // //     }
+// //     // // //     // console.log(counter);
+// //     // // // }) ();
+
+
+// //     // // // console.log(add());
+// //     // // // console.log(add());
+// //     // // // console.log(add());
+// //     // // // // console.log(counter);
+// //     // // // console.log("prince")   
+
+// //     // // function process(){
+// //     // //     console.log("prince")
+// //     // //     setTimeout(function(){
+// //     // //         console.log("sanket")
+// //     // //     },3000);
+// //     // //     for(let i=0;i<1000000000;i++){
+// //     // // }
+// //     // // console.log("prince")
+// //     // // }
+// //     // // process()
+
+// //     // // setTimeout(function(){
+// //     // //     console.log("sanket")
+// //     // // },3000);
+// //     // function exec(){
+// //     // setTimeout(()=>{
+// //     // console.log("sanket")
+// //     // },3000);
+// //     // }
+// //     // exec()
+// //     // function fun(inputstring,fn){
+// //     //     let output=inputstring.split(',');
+// //     //     for(let i=0;i<output.length;i++){
+// //     // fn(output[i]);
+// //     //     fn(output[i]);
+
+// //     //     }
+// //     // }
+
+
+// //     // fun("sanket:prince,shubham:cse",function process(ip){
+// //     //     let arr=ip.split(':');
+// //     //     console.log("{",arr[0],":",arr[1],"}");
+// //     // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //     function exec(inputstring){
+
+// //         var arr=inputstring.split(':');
+// //         console.log(why)
+// //     }
+
+// //     let why=10;
+// //     exec("raj:kumar")
+// //     // let why=10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function download(url,){
+
+
+//     console.log("start downloading")
+
+
+//     setTimeout(function process(){
+//         console.log("downloading from")
+//         let response="this is the file content"
+//         // return response
+//     },3000)
+// }
+
+// // download("www.google.com")
+// let value=download("www.google.com")
+// // console.log(process)
+    
